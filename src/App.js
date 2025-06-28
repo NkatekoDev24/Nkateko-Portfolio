bc313ae1fab8289c677b8a4bc4dcbc77d3aa4569
snkuna685 <snkuna685@gmail.com>
2025-06-28 01:55:04 +0000

    CV download button

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
       <Skills />
       <Projects />
       <Experience />
+      <button href="#" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Download My CV</button>
       <Chatbot />
     </main>
   );
